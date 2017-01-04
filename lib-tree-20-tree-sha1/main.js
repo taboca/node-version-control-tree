89e6296d2e28f093563c8f735c0f81d423b6e26c
Marcio <taboca@gmail.com>
2017-01-04 17:01:51 -0200

    New lib naming structure a bit more educational in order

---
+++ lib-tree-20-tree-sha1/main.js
@@ -0,0 +1,37 @@
+var path = require("path"),
+    fs = require("fs"),
+    crypto = require('crypto'),
+    url = require("url"),
+    traverse = require('../lib-tree-00-json'),
+    traverseFileSha = require('../lib-tree-10-files-sha1');
+
+var traverseTree = {
+
+  run: function (treeItem,l) {
+    var shaWhole = '';
+    if(treeItem.expands == false) {
+       shaWhole = (treeItem.sha1);
+    } else {
+      if(treeItem.tree) {
+        let contentTree = '';
+        l++;
+        for(k in treeItem.tree) {
+            let elect = treeItem.tree[k].shortpath;
+            let shaLeaf = traverseTree.run(treeItem.tree[k],l);
+            contentTree += elect+'/'+shaLeaf+';'
+        }
+        shaWhole = traverseTree.getSha(contentTree);
+        treeItem.treeContent = contentTree;
+        treeItem.sha1 = shaWhole;
+      }
+    }
+    return shaWhole;
+  },
+
+  getSha: function (mString) {
+  		return crypto.createHash('sha1').update(mString).digest('hex');
+  }
+
+}
+
+module.exports = traverseTree;
---
+++ lib-tree-30-index-objects/main.js
@@ -0,0 +1,74 @@
+var path = require("path"),
+    fs = require("fs"),
+    crypto = require('crypto'),
+    url = require("url"),
+    traverse = require('../lib-tree-00-json');
+
+let traverseTree = {
+  gInitial : null,
+  gQueue   : new Array(),
+
+  run: function (treeItem,l,cb) {
+
+    let spacer = '';
+    if(!traverseTree.gInitial) traverseTree.gInitial = treeItem.sha1;
+    for(let k =0; k<l;k++) spacer+='\\_';
+
+    let localTreeItem = treeItem;
+    traverseTree.gQueue.push(treeItem.sha1);
+    traverseTree.writeInIndex(treeItem).then(function () {
+        localTreeItem.indexed = true;
+        traverseTree.gQueue.pop();
+        console.log('___'+ traverseTree.gQueue.length);
+        // Notice that is not going to happen a write for all the files, because
+        // some of the objects can be reused...
+        if(traverseTree.gQueue.length==0) {
+          cb(' indexed all');
+        }
+    });
+
+    if(treeItem.tree) {
+      l++;
+      for(k in treeItem.tree) {
+          traverseTree.run(treeItem.tree[k],l, cb);
+      }
+    }
+  },
+  writeInIndex: function(treeItem) {
+  	return new Promise(function (good,bad) {
+
+      let currFile = path.join(treeItem.fullpath);
+      let indexKeyDirectoryProposal = path.join(__dirname, '..', 'index', treeItem.sha1)
+
+      if (!fs.existsSync(indexKeyDirectoryProposal)){
+
+          fs.mkdirSync(indexKeyDirectoryProposal);
+          let metaJSON = {
+            'filename': treeItem.shortpath,
+            'type':'blob'
+          };
+          if(treeItem.expands == false) {
+          } else {
+            metaJSON.type = 'tree';
+            metaJSON.treeContent = treeItem.treeContent;
+          }
+          let stringTree = JSON.stringify(metaJSON);
+          let metaFile = path.join(__dirname, '..', 'index', treeItem.sha1, 'meta.json');
+          let stream = fs.createWriteStream(metaFile);
+          stream.once('open', function(fd) {
+            //console.log("Writing: " + stringTree )
+            stream.write(stringTree);
+            stream.end();
+            if(treeItem.expands == false) {
+              let blobIn = treeItem.fullpath;
+              let blobFile = path.join(__dirname, '..', 'index', treeItem.sha1, 'blob');
+              fs.createReadStream(blobIn).pipe(fs.createWriteStream(blobFile));
+            }
+            good();
+          });
+      }
+  	});
+  }
+}
+
+module.exports = traverseTree;
